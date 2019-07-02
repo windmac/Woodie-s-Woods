644c83faf0cfee6c1062429b3d67e7b6bf0de83b
Klein <zhaomao.mai@efrei.net>
2019-07-02 16:18:36 +0800

    V 0.0.2
    TopDown视野基本完成，完成上下左右操作

---
+++ Assets/Scirpt/PlayerController2.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+
+
+    public bool isGrounded;
+    public bool isCrouching;
+
+    private float speed = 0;
+    private float w_speed = 0.05f;
+    private float c_speed = 0.025f;
+    public float rotSpeed;
+    public float jumpHeight;
+
+    Rigidbody rb;
+    // Animator anim;
+    BoxCollider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //      anim = GetComponent<Animator>();
+        collider = GetComponent<BoxCollider>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //触发蹲下 按下z
+      /*  if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (isCrouching)
+            {
+                isCrouching = false;
+                //anim.SetBool("isCrouching",false);
+                collider.center = new Vector3(0, 1, 0);
+                // collider.size.Set(collider.size.x, collider.size.y / 2, collider.size.z);
+               // collider.size = new Vector3(1, 1.5f, 1);
+                
+            }
+            else
+            {
+                isCrouching = true;
+                //anim.SetBool("isCrouching",false);
+                collider.center = new Vector3(0, 0.5f, 0);
+                // collider.size.Set(collider.size.x, collider.size.y * 2, collider.size.z);
+               // collider.size = new Vector3(1, 3, 1);
+            }
+        }*/
+
+        var y = Input.GetAxis("Vertical") * speed;
+        var x = Input.GetAxis("Horizontal") * speed;
+
+        transform.Translate(new Vector3(x, 0, y),Space.World);
+        
+
+        Vector3 movement = new Vector3(x, 0.0f, y);
+        transform.rotation = Quaternion.LookRotation(movement);
+        //transform.Translate(0, 0, z);
+        //transform.Rotate(0, y, 0);
+
+        //跳跃 x触发
+        if (Input.GetKey(KeyCode.X) )
+        {
+
+            rb.AddForce(0, jumpHeight, 0);
+            //anim.SetTrigger("isJumping");
+            isCrouching = false;
+            isGrounded = false;
+
+        }
+
+        if (isCrouching)
+        {
+            speed = c_speed;
+        }
+        else if (!isCrouching)
+        {
+            speed = w_speed;
+
+            //以下放动画控制
+        }
+
+
+    }
+
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
+    }
+}
---
+++ Assets/Scirpt/TopDownCamera.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+
+
+        #region Variables
+
+        public Transform target;
+        public float height = 7f;
+        public float distance = 11f;
+        public float angle = 0f;
+        #endregion
+
+        #region Main Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+            HandleCamera();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleCamera();
+        }
+        #endregion
+
+
+        #region Helper Methods
+        protected virtual void HandleCamera()
+        {
+            if(!target)
+            {
+            return;
+            }
+        
+        //Position vector
+        Vector3 worldPosition = (Vector3.forward * -distance) + (Vector3.up * height);
+        Debug.DrawLine(target.position, worldPosition, Color.red);
+
+        //Rotator vector
+
+        Vector3 rotatedVector = Quaternion.AngleAxis(angle, Vector3.up) * worldPosition;
+        Debug.DrawLine(target.position, rotatedVector, Color.blue);
+
+        //Move our position
+        Vector3 flatTargetPosition = target.position;
+        flatTargetPosition.y = 0f;
+        Vector3 finalPosition = flatTargetPosition + rotatedVector;
+
+        Debug.DrawLine(target.position, finalPosition, Color.green);
+        transform.position = finalPosition;
+
+        transform.LookAt(target);
+        }
+        #endregion
+
+}
