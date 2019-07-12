9c4f27bf8ad4d19ae9adae8675606f9335f1b777
Cuusint <1679395259@qq.com>
2019-07-12 21:56:30 +0800

    场景_任务
    场景结构重组，任务系统开发

---
+++ Assets/Scirpt/GameManagement/MissionNode.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MissionState
+{
+    Unacceptable = 0,
+    Acceptable = 1,
+    InProgress = 2,
+    SuccessfullyFinished = 3,
+    FailedMission = 4,
+}
+
+public class MissionNode
+{
+    public string MissionID;
+    public MissionState State { get; protected set; }
+    public string MissionName;
+    public string MissionDescription;
+    public string NextMissionID;
+
+    public MissionNode(string missionID,MissionState state ,string missionName,string missionDescription,string nextMissionID)
+    {
+        MissionID = missionID;
+        SetMissionState(state);
+        MissionName = missionName;
+        MissionDescription = missionDescription;
+        NextMissionID = nextMissionID;
+    }
+
+    public void SetMissionState(MissionState state)
+    {
+        if ((int)state >= (int)State)     //任务不能倒着进行
+        {
+            State = state;
+        }
+        else
+        {
+            return;
+        }
+        switch (state)
+        {
+            case MissionState.Unacceptable:
+                //WHAT TO DO
+                break;
+            case MissionState.Acceptable:
+                break;
+            case MissionState.InProgress:
+                break;
+            case MissionState.SuccessfullyFinished:
+                break;
+            case MissionState.FailedMission:
+                break;
+            default:
+                break;
+        }
+    }
+}
